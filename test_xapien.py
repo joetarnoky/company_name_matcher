125da644d4fb526ea0b100c241da830b48b2bead
joetarnoky <91961350+joetarnoky@users.noreply.github.com>
2023-08-07 15:42:48 +0100

    Update test_xapien.py

--- test_xapien.py
+++ test_xapien.py
@@ -6,6 +6,8 @@ class TestCustomClean_name(unittest.TestCase):
     def test_clean_name(self):
         self.assertEqual(clean_name("  The XYZ Holdings"), "xyz")
         self.assertEqual(clean_name("t-he Caprice, holdings. ltd "), "caprice")
+        self.assertEqual(clean_name("999999, "), "999999")
+        self.assertEqual(clean_name(" Holdings  "), "")
 
 class TestLoadAndCleanData(unittest.TestCase):
     def test_load_and_clean_data(self):
@@ -13,6 +15,7 @@ class TestLoadAndCleanData(unittest.TestCase):
         self.assertEqual(cleaned_names[0], "caprice")
         self.assertEqual(cleaned_names[1], "nashville")
         self.assertEqual(unique_original_names[1], "THE NASHVILLE CORPORATION LIMITED")
+        self.assertEqual(unique_original_names[-2], "")
 
     def test_empty_json_file(self):
         with open('empty_file.json', 'w') as json_file:
@@ -36,7 +39,7 @@ class TestFindIndexPairs(unittest.TestCase):
     def test_find_index_pairs(self):
         company_names = ["caprice", "nashville", "olswang", "caprice", "nashville", ""]
         jaccard_threshold = 0.1  
-        fuzzy_threshold = 80  
+        fuzzy_threshold = 70  
         index_pairs = find_index_pairs(company_names, jaccard_threshold, fuzzy_threshold)
         self.assertEqual(index_pairs, [(0, 3), (1, 4)])
         
